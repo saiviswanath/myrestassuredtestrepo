01181e727608294f6e2f5c05a1d1c194ab9444ed
Viswa <viswa@c.com>
2024-11-07 13:58:52 +0530

    logs Signed-off-by: Viswa <viswa@c.com>

--- src/test/java/com/xyz/base/utils/Log4jFilter.java
+++ src/test/java/com/xyz/base/utils/Log4jFilter.java
@@ -22,43 +22,56 @@ public class Log4jFilter implements Filter {
         Response response = filterContext.next(reqSpec, resSpec);
 
         logger.info("Method: {}", reqSpec.getMethod());
+        Report.infoStep("Method: " + reqSpec.getMethod());
         logger.info("URI: {}", reqSpec.getURI());
+        Report.infoStep("URI:  " + reqSpec.getURI());
 
         logger.info("Request Headers:");
         logger.info(reqSpec.getHeaders());
+        Report.infoStep("Request Headers: " + reqSpec.getHeaders().toString());
 
         if (!reqSpec.getPathParams().isEmpty()) {
             logger.info("Request Path Parameters:");
+            Report.infoStep("Request Path Parameters:");
             logMap(reqSpec.getPathParams());
         }
 
         if (!reqSpec.getQueryParams().isEmpty()) {
             logger.info("Request Query Parameters:");
+            Report.infoStep("Request Query Parameters:");
             logMap(reqSpec.getQueryParams());
         }
 
         if (!reqSpec.getFormParams().isEmpty()) {
             logger.info("Request Form Parameters:");
+            Report.infoStep("Request Form Parameters:");
             logMap(reqSpec.getFormParams());
         }
 
         if (reqSpec.getBody() != null) {
             logger.info("Request Body:");
             logger.info(reqSpec.getBody().toString());
+            Report.infoStep("Request Body: " + reqSpec.getBody().toString());
         }
 
         logger.info("Response Status Code: {}", response.statusCode());
+        Report.infoStep("Response Status Code: " + response.statusCode());
 
         logger.info("Response Headers:");
         logger.info(response.getHeaders());
+        Report.infoStep("Response Headers: " + response.getHeaders());
 
         logger.info("Response Body:");
         logger.info(response.getBody().asPrettyString());
+        Report.infoStep("Response Body: " + response.getBody().asPrettyString());
 
         return response;
     }
 
     private void logMap(Map<String, String> map) {
-        map.forEach((key, value) -> logger.info("{} : {}", key, value));
+        map.forEach((key, value) -> {
+        	logger.info("{} : {}", key, value);
+        	Report.infoStep(key + "-" + value);
+        });
     }
 }
